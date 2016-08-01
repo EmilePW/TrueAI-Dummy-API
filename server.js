7670f62dbe011e93c8e8b11aa11375c18f360e28
EmilePW <emilepw@hotmail.co.uk>
2016-08-01 21:59:24 +0100

    Return an array of three responses instead of just one as a string

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ var router = express.Router();
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/suggestions', function(req, res) {
     if (req.query.q && req.query.company) {
-      var response = 'Hi there, how can I help?';
+      var responses = [
+        'Hi there, how can I help?',
+        'Have you tried turning it off and on again?',
+        'What seems to be the issue?'
+      ]
 
       // Return the same basic response
-      res.json(response);
+      res.json(responses);
     }
 });
 
